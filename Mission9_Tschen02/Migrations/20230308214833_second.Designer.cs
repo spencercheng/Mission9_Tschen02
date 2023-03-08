0c5f2521afe7b9ca75eba420036e4415d22dca5c
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2023-03-08 15:21:32 -0700

    database

--- Mission9_Tschen02/Migrations/20230308214833_second.Designer.cs
+++ Mission9_Tschen02/Migrations/20230308214833_second.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mission9_Tschen02.Models;
 
 namespace Mission9_Tschen02.Migrations
 {
     [DbContext(typeof(BookstoreContext))]
-    partial class BookstoreContextModelSnapshot : ModelSnapshot
+    [Migration("20230308214833_second")]
+    partial class second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mission9_Tschen02/Migrations/20230308214833_second.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission9_Tschen02.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketLineItem_Donations_DonationId",
+                table: "BasketLineItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Donations",
+                table: "Donations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BasketLineItem_DonationId",
+                table: "BasketLineItem");
+
+            migrationBuilder.DropColumn(
+                name: "DonationId",
+                table: "Donations");
+
+            migrationBuilder.DropColumn(
+                name: "DonationId",
+                table: "BasketLineItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PurchaseId",
+                table: "Donations",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PurchaseId",
+                table: "BasketLineItem",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Donations",
+                table: "Donations",
+                column: "PurchaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketLineItem_PurchaseId",
+                table: "BasketLineItem",
+                column: "PurchaseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketLineItem_Donations_PurchaseId",
+                table: "BasketLineItem",
+                column: "PurchaseId",
+                principalTable: "Donations",
+                principalColumn: "PurchaseId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketLineItem_Donations_PurchaseId",
+                table: "BasketLineItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Donations",
+                table: "Donations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BasketLineItem_PurchaseId",
+                table: "BasketLineItem");
+
+            migrationBuilder.DropColumn(
+                name: "PurchaseId",
+                table: "Donations");
+
+            migrationBuilder.DropColumn(
+                name: "PurchaseId",
+                table: "BasketLineItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DonationId",
+                table: "Donations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DonationId",
+                table: "BasketLineItem",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Donations",
+                table: "Donations",
+                column: "DonationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketLineItem_DonationId",
+                table: "BasketLineItem",
+                column: "DonationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketLineItem_Donations_DonationId",
+                table: "BasketLineItem",
+                column: "DonationId",
+                principalTable: "Donations",
+                principalColumn: "DonationId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
