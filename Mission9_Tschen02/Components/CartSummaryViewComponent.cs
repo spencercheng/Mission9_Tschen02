60606ebb992b8384534356e105046791b7c61e37
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2023-03-08 17:33:42 -0700

    Icon and Store

---
+++ Mission9_Tschen02/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission9_Tschen02.Models;
+
+namespace Mission9_Tschen02.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket cart;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
